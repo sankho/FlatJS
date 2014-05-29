dd90f19b5682a4370801f7b99d9974adb47d47d5
Sankho Mallik <sankho.mallik@gmail.com>
2014-05-29 00:05:41 -0400

    updates dispatch w/ awareness of context + classy extensions

--- src/FlatJS.Dispatch.js
+++ src/FlatJS.Dispatch.js
@@ -4,43 +4,85 @@
 FlatJS.Dispatch = (function() {
 
   var api         = {},
-      pubSubCache = {};
+      cache       = {};
 
   api.publish = function(topic, args) {
-    if (pubSubCache[topic]) {
-      for (var i = 0; i < pubSubCache[topic].length; i++) {
-        pubSubCache[topic][i].apply(this, args);
+    if (cache[topic]) {
+      for (var i = 0; i < cache[topic].length; i++) {
+        if (cache[topic][i]) {
+          cache[topic][i].fn.apply(this, args);
+        }
       }
     }
   }
 
   api.subscribe = function(topic, callback) {
-    if(!pubSubCache[topic]){
-      pubSubCache[topic] = [];
+    if(!cache[topic]){
+      cache[topic] = [];
     }
-    pubSubCache[topic].push(callback);
-    return [topic, callback]; // Array
+    cache[topic].push({
+      instance: this,
+      fn:       callback
+    });
+    // return [topic, callback]; // Array
   };
 
+  // TODO: Make shorter / separate into private functions.
   api.unsubscribe = function(topic, callback){
-    if (pubSubCache[topic]) {
+    if (cache[topic]) {
       if (callback) {
-        for (var i = 0; i < pubSubCache[topic].length; i++) {
-          var fn = pubSubCache[topic][i];
-          if(fn && fn === callback){
-            pubSubCache[topic].splice(fn, 1);
+        for (var i = 0; i < cache[topic].length; i++) {
+          var obj = cache[topic][i];
+          if(obj && obj.fn && obj.fn === callback){
+            cache[topic].splice(obj, 1);
           }
         }
       } else {
-        pubSubCache[topic] = [];
+        if (this !== api) {
+          for (var i = 0; i < cache[topic].length; i++) {
+            var obj = cache[topic][i];
+            if (obj && obj.instance && obj.instance === this) {
+              cache[topic].splice(obj, 1)
+            }
+          }
+        } else {
+          cache[topic] = [];
+        }
+      }
+    } else if (!topic && !callback && this !== api) {
+      for (var topicKey in cache) {
+        var topic = cache[topicKey];
+        for (var cb in topic) {
+          var obj = topic[cb];
+
+          if (typeof obj.fn === 'function' && obj.instance === this) {
+            topic[cb] = undefined;
+            delete topic[cb];
+          }
+        }
       }
     }
   }
 
   if (FlatJS.Classy) {
-    FlatJS.Classy.prototype.publish = function() {}
-    FlatJS.Classy.prototype.subscribe = function() {}
-    FlatJS.Classy.prototype.unsubscribe = function() {}
+
+    FlatJS.Classy.prototype.publish = function(topic, args) {
+      this._(api.publish)(topic, args);
+    }
+
+    FlatJS.Classy.prototype.subscribe = function(topic, callback) {
+      this._(api.subscribe)(topic, callback);
+    }
+    
+    FlatJS.Classy.prototype.unsubscribe = function(topic, callback) {
+      this._(api.unsubscribe)(topic, callback);
+    }
+  }
+
+  if (FlatJS.Widget) {
+    FlatJS.Widget.prototype.destroy = function () {
+      this.unsubscribe();
+    };
   }
 
   return api;
