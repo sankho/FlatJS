7c6859c28463c6bc7ab8eb8e2ad1707dc0e13c64
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-10 03:25:56 -0400

    adds an asynchronous test

--- tests/Object.tests.js
+++ tests/Object.tests.js
@@ -7,3 +7,23 @@ test("FlatJS.Object existence tests", function(assert) {
   ok(FlatJS.Object.prototype instanceof FlatJS.Classy, 'FlatJS.Object inherits FlatJS.Classy');
 
 });
+
+asyncTest("FlatJS.Object setter & watch functionality", function(assert) {
+
+  var x = new FlatJS.Object();
+
+  function watchCallback(name, oldVal, val) {
+    ok(true, "callback on watch function successful");
+    equal(val, 3, "callback on watch function successfully returns changed new value");
+    equal(oldVal, undefined, "callback on watch function successfully retains old value");
+    equal(name, 'g', "callback on watch function successfully retains name of property");
+    QUnit.start();
+  }
+
+  x.watch('g', watchCallback);
+
+  x.set('g', 3);
+
+  equal(x.g, 3, "x.set() sets a variable on the object, successfully mutes actual object");
+
+});
