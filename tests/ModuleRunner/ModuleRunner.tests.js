28df2545bad7beb185f8a959a7b02cd8e3640e1b
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-10 22:59:40 -0400

    adjusts tests to seek correct key values

--- tests/ModuleRunner/ModuleRunner.tests.js
+++ tests/ModuleRunner/ModuleRunner.tests.js
@@ -22,13 +22,13 @@ var __moduleRunnerMockData = {
     }
   }),
 
-  basicImplentationTests: function(mock, obj2) {
+  basicImplentationTests: function(mock, obj2, nodeSet) {
       QUnit.ok(mock.jsModules, "jsModules array attaches to <div> on init, init runs by default, works without provided scope");
-      QUnit.ok(mock.jsModules['__moduleRunnerMockData.moduleOne'] instanceof __moduleRunnerMockData.moduleOne, "moduleOne module executed on div, retrieves object and is instance of __moduleRunnerMockData.moduleOne");
+      QUnit.ok(mock.jsModules[!nodeSet ? '__moduleRunnerMockData.moduleOne' : 'moduleOne'] instanceof __moduleRunnerMockData.moduleOne, "moduleOne module executed on div, retrieves object and is instance of __moduleRunnerMockData.moduleOne");
 
-      QUnit.ok(obj2.jsModules, "jsModules array attaches to <div> on inner <div> recursively")
-      QUnit.ok(obj2.jsModules['__moduleRunnerMockData.moduleTwo'] instanceof __moduleRunnerMockData.moduleTwo, "moduleTwo is assigned appropriately to inner <div>")
-      QUnit.equal(obj2.jsModules['__moduleRunnerMockData.moduleTwo'].int1, 23, "reference to inner object returns correctly set variable retrieved from data attribute")
+      QUnit.ok(obj2.jsModules, "jsModules array attaches to <div> on inner <div> recursively");
+      QUnit.ok(obj2.jsModules[!nodeSet ? '__moduleRunnerMockData.moduleTwo' : 'moduleTwo'] instanceof __moduleRunnerMockData.moduleTwo, "moduleTwo is assigned appropriately to inner <div>");
+      QUnit.equal(obj2.jsModules[!nodeSet ? '__moduleRunnerMockData.moduleTwo' : 'moduleTwo'].int1, 23, "reference to inner object returns correctly set variable retrieved from data attribute");
   }
 
 }
@@ -97,7 +97,7 @@ __moduleRunnerMockData.mockLoaded = function() {
 
     runner.init();
 
-    __moduleRunnerMockData.basicImplentationTests(mock, obj2);
+    __moduleRunnerMockData.basicImplentationTests(mock, obj2, true);
   });
 
 };
