16f7e96c8b0da9f80d794b7d0ce289cc5b1e82ef
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-11 19:10:18 -0400

    adds basic scaffold and private functions to MV laying out initial callstack

--- src/FlatJS.MV.js
+++ src/FlatJS.MV.js
@@ -2,8 +2,27 @@ var FlatJS = FlatJS || {};
 
 FlatJS.MV = FlatJS.Widget.extend(function() {
 
-  return {
+  var api = {
+
+    render: function() {
+      this._(findAndInitializeModels)();
+      this.initializer();
+      this.renderUI();
+      this.syncUI();
+      this._(bindMVKeys);
+      this.bindUI();
+    }
 
   };
+
+  function findAndInitializeModels() {
+
+  }
+
+  function bindMVKeys() {
+
+  }
+
+  return api;
 
 });
