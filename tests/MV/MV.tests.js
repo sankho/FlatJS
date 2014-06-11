7c15fb798809dfc4cad583c922571d5a06758899
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-11 17:28:54 -0400

    cleaner HTML based implementation, writes tests that check for model generation based on provided markup

--- tests/MV/MV.tests.js
+++ tests/MV/MV.tests.js
@@ -1,8 +1,11 @@
 // should contain all unit tests on FlatJS.MV.js
 
 var __MVMockData = {
-  HTML: ''
+  HTML: '',
+  customMV: FlatJS.MV || {}
 }
+
+var APP = {};
 
 $.ajax({
 
@@ -18,19 +21,25 @@ $.ajax({
 
 __MVMockData.mockLoadedCallback = function() {
 
+  // init the MV default behavoir in mock
+  new FlatJS.ModuleRunner().init();
+
+  var $mock = $('#flat-mv-test-mock'),
+      mock  = $mock.get(0),
+      mvMod = mock.jsModules['FlatJS.MV'];
+
   QUnit.test("FlatJS.MV existence tests", function() {
-
     var $mock = $('#flat-mv-test-mock');
-
     QUnit.equal($mock.length, 1, 'Mock successfully AJAXed in');
     QUnit.equal(typeof FlatJS.MV, 'function', "FlatJS.MV exists, is a function");
-    QUnit.equal(typeof FlatJS.MV.extend, 'function', 'FlatJS.MV.extend exists, is a function');
-
+    QUnit.ok(FlatJS.MV instanceof FlatJS.Widget, 'FlatJS.MV is an instance of FlatJS.Widget');
   });
 
-  QUnit.test("FlatJS.MV tests - basic functionality", function() {
+  QUnit.test("FlatJS.MV tests - DOM loaded model generation", function() {
 
-
+    QUnit.equal(typeof APP.Person, 'object', "APP.Person & APP.Todo model references should be automatically generated");
+    QUnit.equal(typeof APP.Todo, 'object', "APP.Person & APP.Todo model references should be automatically generated");
+    QUnit.equal(APP.PERSON.objects.length, 2, "APP.PERSON.objects has 2 people in it");
 
   });
 
