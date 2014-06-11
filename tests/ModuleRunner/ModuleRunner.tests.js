acedd1d103fe55529652df0f30099ca05506ad80
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-10 21:32:04 -0400

    adds basic tests for module runner + mock html

--- tests/ModuleRunner/ModuleRunner.tests.js
+++ tests/ModuleRunner/ModuleRunner.tests.js
@@ -1,5 +1,22 @@
 // tests for module runner
 
+var mockHTML = '';
+
+var moduleOne = function(obj) {
+  this.int1 = 33;
+  this.int2 = 55;
+
+  obj.innerHTML = obj.innerHTML + '<p>Module One.</p>';
+}
+
+var moduleTwo = FlatJS.Classy.extend({
+  init: function(obj) {
+    this.int1 = 23;
+    this.int2 = 89;
+
+    obj.innerHTML = obj.innerHTML + '<p>Module Two.</p>';
+  }
+});
 
 QUnit.asyncTest("FlatJS.ModuleRunner Loads Mock File", function(assert) {
 
@@ -7,27 +24,33 @@ QUnit.asyncTest("FlatJS.ModuleRunner Loads Mock File", function(assert) {
 
     url: 'ModuleRunner/mock.html',
     success: function(data) {
-      $('#mock-area').append(data);
+      mockHTML = data;
+      $('#mock-area').append(mockHTML);
 
       mockLoaded();
     }
 
   })
 
-  function mockLoaded() {
-
-    QUnit.ok($('#module-runner-test-mock').length > 0, "Module runner mock HTML loads and appends via ajax");
-
-    QUnit.start();
-
-  }
-
 });
 
-QUnit.asyncTest("FlatJS.ModuleRunner default functionality", function(assert) {
+
+
+function mockLoaded() {
+
+  QUnit.ok($('#module-runner-test-mock').length > 0, "Module runner mock HTML loads and appends via ajax");
 
   QUnit.start();
 
+  QUnit.test("FlatJS.ModuleRunner default functionality", function(assert) {
 
+    var $mock = $('#module-runner-test-mock'),
+        mock  = $mock.get(0);
 
-});
+    var runner = new FlatJS.ModuleRunner();
+
+    QUnit.ok(mock.jsModules, "jsModules array attaches to <div> on init, init runs by default");
+    QUnit.ok(mock.jsModules['moduleOne'] instanceof moduleOne, "moduleOne module executed on div");
+
+  });
+}
