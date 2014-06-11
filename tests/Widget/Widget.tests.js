78580b40402bf3ee98ee6590a10f6ff99eacdb1a
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-11 00:15:45 -0400

    adds tests to ensure rendering widgets kicks off their lifecycle

--- tests/Widget/Widget.tests.js
+++ tests/Widget/Widget.tests.js
@@ -1,7 +1,23 @@
 // tests for FlatJS.Widget
 
 var __widgetMockData = {
-  HTML: ''
+
+  HTML: '',
+  exampleWidget: FlatJS.Widget.extend({
+    initializer: function() {
+      this.initialized = true;
+    },
+    renderUI: function() {
+      this.renderUIed = true;
+    },
+    syncUI: function() {
+      this.syncUIed = true;
+    },
+    bindUI: function() {
+      this.bindUIed = true;
+    }
+  })
+
 };
 
 QUnit.asyncTest("FlatJS.Widget - Loads Mock File", function(assert) {
@@ -30,6 +46,11 @@ __widgetMockData.mockLoaded = function() {
   QUnit.equal($mock.length, 1, "Mock object exists in DOM")
 
   QUnit.test("FlatJS.Widget - Tests lifecycle", function() {
+    var x = new __widgetMockData.exampleWidget($mock.get);
 
+    QUnit.equal(x.initialized, true, "x.initializer() fires on object construction");
+    QUnit.equal(x.renderUIed, true, "x.renderUI() fires on object construction");
+    QUnit.equal(x.syncUIed, true, "x.syncUI() fires on object construction");
+    QUnit.equal(x.bindUIed, true, "x.bindUI() fires on object construction");
   });
 }
