4b5cc0ed7b95c3c13890d76ffb59bd2d0ab8a405
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-13 18:32:30 -0400

    using the things I create

--- src/FlatJS.MV.js
+++ src/FlatJS.MV.js
@@ -101,8 +101,8 @@ FlatJS.MV = FlatJS.Widget.extend(function() {
     for (var i = 0; i < nodes.length; i++) {
       var node = nodes[i];
 
-      if (!node.object._FJSwatchSet) {
-        node.object._FJSwatchSet = true;
+      if (!node.object._('FJSwatchSet')) {
+        node.object._('FJSwatchSet', true);
         node.object.watch(node.getAttribute('data-mv-key'), this._(syncMVKeyOnObjectChange));
       }
     }
