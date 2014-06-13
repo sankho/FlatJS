fb6b9664c45a884e4679a5710f1bbaedef33ecea
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-13 11:11:05 -0400

    adds methods for generating model data from markup, writes method to get key / value pairs from tags labeled with data-mv-key

--- src/FlatJS.MV.js
+++ src/FlatJS.MV.js
@@ -5,11 +5,13 @@ FlatJS.MV = FlatJS.Widget.extend(function() {
   var api = {
 
     render: function() {
-      this._(findAndInitializeModels)();
       this.initializer();
+      this._(findAndInitializeModels)();
+      this._(syncMVKeys)();
+      this._(assembleJSON)();
       this.renderUI();
       this.syncUI();
-      this._(bindMVKeys);
+      this._(bindMVKeys)();
       this.bindUI();
     }
 
@@ -37,6 +39,29 @@ FlatJS.MV = FlatJS.Widget.extend(function() {
   }
 
   function stripDataFromNodeAndUpdateObject(obj, node) {
+    var children = node.childNodes;
+
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+
+      if (child && child.hasAttribute && child.hasAttribute('data-mv-key')) {
+        this._(getKeyAndValueFromNodeAndAddToObject)(obj, child);
+      }
+    }
+  }
+
+  function getKeyAndValueFromNodeAndAddToObject(obj, node) {
+    var key = node.getAttribute('data-mv-key'),
+        val = node.innerHTML;
+
+    obj.set(key, val);
+  }
+
+  function syncMVKeys() {
+
+  }
+
+  function assembleJSON() {
 
   }
 
