ceb6e6e9b040b17316fb3b4ea58e5f7b76bb6b70
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-22 15:53:05 -0400

    adds limited todo add functionality... need to figure out HTML templating w/ classes a bit better. completion is funny.

--- examples/todomvc/js/app.js
+++ examples/todomvc/js/app.js
@@ -2,12 +2,39 @@
   'use strict';
 
   FlatTodo.TodoHandler = FlatJS.MV.extend(function() {
-    return {
+    var api = {
       initializer: function() {
-        this._('newTodoInput', document.getElementById('new-todo'));
+        this._('$newTodoInput', $('#new-todo'));
+        this._('$todoList', $('#todo-list'));
+      },
+
+      bindUI: function() {
+        this._('$newTodoInput').on('keyup', this._(addNewTodoOnEnter));
       }
     }
-  })
+
+    function addNewTodoOnEnter(e) {
+      var keycode = (e.keyCode ? e.keyCode : e.which),
+          val     = this._('$newTodoInput').val();
+
+      if (keycode == '13') {
+        this._('$newTodoInput').val('');
+
+        var todo = new FlatTodo.Todo({
+          // IDs could be auto generated
+          id: FlatTodo.Todo.objects.length+1,
+          text: val
+        });
+
+        this.JSON.todosList.push(todo);
+        this.renderFromJSON();
+        this.initializer();
+        this.bindUI();
+      }
+    }
+
+    return api;
+  });
 
   FlatTodo.runner = new FlatJS.ModuleRunner({
     context: FlatTodo
