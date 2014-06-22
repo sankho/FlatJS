d1b5e85308b762f7c0d8027e8b3bd3d43bbdfdd8
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-22 15:07:14 -0400

    adds namespace + initial MV widget to store all app functionality in one housing relative to the single <section> the app is scoped under in the DOM

--- examples/todomvc/js/app.js
+++ examples/todomvc/js/app.js
@@ -1,6 +1,25 @@
 (function (window) {
 	'use strict';
 
-  FlatTodo.runner = new FlatJS.ModuleRunner
+  FlatTodo.runner = new FlatJS.ModuleRunner();
 
 })(window);
+
+
+
+
+
+
+FlatTodo.TodoApp = FlatJS.MV.extend(function() {
+
+  var app = {
+
+    initializer: function() {
+
+    }
+
+  };
+
+  return app;
+
+})
