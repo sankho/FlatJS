b03d92045276358a4e33e8f345aa56f7b6f2465c
Sankho Mallik <sankho.mallik@gmail.com>
2014-06-30 18:46:06 -0400

    initial refactor; making an "old" file to save stuff (dunno why I'm using git then, but whatever), taking out all notions of a template + building it from JSON, changing render lifecycle to represent that. .render should only be called if all the HTML has been replaced for some reason. now to work on better binding.

--- src/FlatJS.Component.Old.js
+++ src/FlatJS.Component.Old.js
@@ -10,31 +10,62 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
     key:      'fjs-key',
     array:    'fjs-array',
     object:   'fjs-obj'
-  },
-  convertCamelCase = FlatJS.Helpers.convertDashedToCamelCase;
+  }
 
   var api = {
 
     fjsTmpl: undefined,
     fjsData: undefined,
 
-    render: function() {
+    init: function(node) {
+      this.obj = node;
       this._(internalInitializer)();
-      this._(findAndInitializeResources)();
+      this._(findAndInitializeModels)();
       this._(assembleJSON)();
-      this._(applyCSSChanges)();
-      this._(bindMVKeys)();
+      this._(createTemplateFromMarkup)();
       this._(syncMVKeys)();
+      this._super(node);
+    },
+
+    render: function() {
       this.initializer();
       this.renderUI();
+      this._(findAndInitializeModels)();
+      this._(applyCSSChanges)();
       this.syncUI();
+      this._(bindMVKeys)();
       this.bindUI();
+    },
+
+    updateJSON: function(obj) {
+      this.fjsData = this.fjsData.extend(obj);
+      return this.fjsData;
+    },
+
+    renderFromJSON: function(node, parentObj, returnParent) {
+      node         = node || this.fjsTmpl;
+      parentObj    = parentObj || this.fjsData;
+      var children = node.childNodes;
+
+      for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        if (child.hasAttribute) {
+          this._(renderJSONOntoNode)(child, parentObj);
+        }
+      }
+
+      if (returnParent) {
+        return node;
+      } else {
+        this.obj.textContent ? this.obj.textContent = '' : this.obj.innerHTML = '' ;
+        this.obj.appendChild(this.fjsTmpl);
+      }
     },
 
     findResourceFromNode: function(node) {
       if (node.hasAttribute && node.hasAttribute(ATTR.resource) && node.hasAttribute(ATTR.id)) {
         // ugly.
-        var model = findFn(node.getAttribute(ATTR.resource));
+        var model = FlatJS.Helpers.findFunctionByString(FlatJS.Helpers.convertDashedToCamelCase(node.getAttribute(ATTR.resource)));
 
         return model.find(node.getAttribute(ATTR.id));
       } else if (node === document) {
@@ -43,16 +74,7 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
         return this.findResourceFromNode(node.parentNode);
       }
     }
-  }
-
-
-  function findFn(string) {
-    return FlatJS.Helpers.findFunctionByString(convertCamelCase(string));
-  }
-
-  function internalInitializer() {
-    this.fjsData = this.fjsData || new FlatJS.Resource();
-  }
+  };
 
   function applyCSSChanges() {
     var nodes = FlatJS.Helpers.getAllElementsWithAttribute(ATTR.class, this.obj);
@@ -115,7 +137,104 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
     }
   }
 
-  function findAndInitializeResources() {
+  function renderJSONArrayOntoNode(arr, cnnr) {
+    var children = cnnr.childNodes,
+        tmpl     = false,
+        nodes    = [];
+
+    for (var i = 0; i < children.length && !tmpl; i++) {
+      var child = children[i];
+
+      if (child.hasAttribute) {
+        tmpl = child.cloneNode(true);
+      }
+    }
+
+    cnnr.innerHTML = '';
+
+    for (var j = 0; j < arr.length; j++) {
+      var _tmpl = tmpl.cloneNode(true),
+          node  = this.renderFromJSON(_tmpl, arr[j], true);
+      if (arr[j].id) {
+        node.setAttribute(ATTR.id, arr[j].id);
+      }
+      cnnr.appendChild(node);
+    }
+  }
+
+  function renderJSONOntoNode(child, parentObj) {
+    var key;
+
+    if (child.hasAttribute(ATTR.key)) {
+      key = FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(ATTR.key));
+      this._(setValueOnNode)(child, parentObj[key]);
+      if (parentObj[key] == true && (child.getAttribute('type') === 'radio' || child.getAttribute('type') === 'checkbox')) {
+        parentObj[key] = this._(getValueFromNode)(child);
+      }
+    } else if (child.hasAttribute(ATTR.object)) {
+      key = FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(ATTR.object));
+      this.renderFromJSON(child, parentObj[key]);
+    } else if (child.getAttribute(ATTR.resource)) {
+      key = FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(ATTR.resource));
+      if (parentObj[key].id) {
+        child.setAttribute(ATTR.id, parentObj[key].id);
+      }
+      this.renderFromJSON(child, parentObj[key]);
+    } else if (child.hasAttribute(ATTR.array)) {
+      var key = FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(ATTR.array)),
+          arr = parentObj[key];
+      if (arr && child.childNodes) {
+        this._(renderJSONArrayOntoNode)(arr, child);
+      }
+    } else if (child.childNodes && child.childNodes.length > 0) {
+      this.renderFromJSON(child, parentObj);
+    }
+  }
+
+  function internalInitializer() {
+    this.fjsData = this.fjsData || new FlatJS.Resource();
+  }
+
+  function createTemplateFromMarkup(tmplNode) {
+    var tmpl     = tmplNode ? tmplNode : this.obj.cloneNode(true),
+        children = tmpl.childNodes;
+    this.fjsTmpl = this.fjsTmpl || tmpl;
+
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i];
+
+      if (child.hasAttribute) {
+        if (child.hasAttribute(ATTR.key) || child.hasAttribute(ATTR.key)) {
+          this._(setValueOnNode)(child, '');
+        } else if (child.hasAttribute(ATTR.id)) {
+          child.removeAttribute(ATTR.id);
+        } else if (child.hasAttribute(ATTR.array)) {
+          this._(pruneArrayToOneElementForTemplate)(child);
+        }
+      }
+
+      if (child.childNodes && child.childNodes.length > 0) {
+        this._(createTemplateFromMarkup)(child);
+      }
+    }
+  }
+
+  function pruneArrayToOneElementForTemplate(child) {
+    var children = child.childNodes,
+        gotFirst = false;
+
+    for (var i = 0; i < children.length; i++) {
+      var _child = children[i];
+
+      if (_child.hasAttribute && !gotFirst) {
+        gotFirst = true;
+      } else if (_child.hasAttribute) {
+        _child.parentNode.removeChild(_child);
+      }
+    }
+  }
+
+  function findAndInitializeModels() {
     var modelNodes = FlatJS.Helpers.getAllElementsWithAttribute(ATTR.resource, this.obj);
 
     for (var i = 0; i < modelNodes.length; i++) {
@@ -127,7 +246,7 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
 
   function createModelObjectFromNode(node) {
     var id        = node.getAttribute(ATTR.id),
-        modelName = convertCamelCase(node.getAttribute(ATTR.resource)),
+        modelName = FlatJS.Helpers.convertDashedToCamelCase(node.getAttribute(ATTR.resource)),
         model     = FlatJS.Helpers.findFunctionByString(
                       modelName,
                       window,
@@ -140,7 +259,7 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
     obj.modelName  = obj.modelName  || modelName;
     obj._('fjsNodes', obj._('fjsNodes') || []);
     obj._('fjsNodes').push(node);
-    node.fjsObject = obj;
+    node.object = obj;
 
     if (node.hasAttribute(ATTR.json)) {
       obj.extend(JSON.parse(node.getAttribute(ATTR.json)));
@@ -175,7 +294,7 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
 
   function checkArrayForModelRelations(obj, node) {
     var children  = node.childNodes,
-        key       = convertCamelCase(node.getAttribute(ATTR.array)),
+        key       = FlatJS.Helpers.convertDashedToCamelCase(node.getAttribute(ATTR.array)),
         parentObj = obj[key] = obj[key] || [];
 
     for (var i = 0; i < children.length; i++) {
@@ -191,10 +310,10 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
   }
 
   function getKeyAndValueFromNodeAndAddToObject(obj, node) {
-    var key = convertCamelCase(node.getAttribute(ATTR.key)),
+    var key = FlatJS.Helpers.convertDashedToCamelCase(node.getAttribute(ATTR.key)),
         val = this._(getValueFromNode)(node);
 
-    node.fjsObject = obj;
+    node.object = obj;
     obj._('fjsNodes').push(node);
     obj[key] = val;
   }
@@ -214,9 +333,9 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
 
     for (var i = 0; i < nodes.length; i++) {
       var node = nodes[i];
-      if (node && node.fjsObject) {
-        key  = convertCamelCase(node.getAttribute(ATTR.key)),
-        attr = node.fjsObject[key];
+      if (node && node.object) {
+        key  = FlatJS.Helpers.convertDashedToCamelCase(node.getAttribute(ATTR.key)),
+        attr = node.object[key];
         if (attr) {
           this._(setValueOnNode)(node, attr);
         }
@@ -230,9 +349,9 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
     for (var i = 0; i < nodes.length; i++) {
       var node = nodes[i];
 
-      if (node && node.fjsObject && !node.fjsObject._('fjsWatchSet')) {
-        node.fjsObject._('fjsWatchSet', true);
-        node.fjsObject.watch(node.getAttribute(ATTR.key), this._(syncMVKeyOnObjectChange));
+      if (node && node.object && !node.object._('fjsWatchSet')) {
+        node.object._('fjsWatchSet', true);
+        node.object.watch(node.getAttribute(ATTR.key), this._(syncMVKeyOnObjectChange));
       }
     }
   }
@@ -249,7 +368,7 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
       if (child.hasAttribute) {
         var isModel = child.hasAttribute(ATTR.resource);
         if (child.hasAttribute(ATTR.key)) {
-          var key = convertCamelCase(child.getAttribute(ATTR.key));
+          var key = FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(ATTR.key));
           if (child.getAttribute('type') === 'radio') {
             parentObj[key] = parentObj[key] || this._(getValueFromNode)(child);
           } else {
@@ -269,7 +388,7 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
   function constructJSONfromArray(cnnr, parentObj, parentIsArray) {
     var children  = cnnr.childNodes,
         stop      = false,
-        key       = convertCamelCase(cnnr.getAttribute(ATTR.array));
+        key       = FlatJS.Helpers.convertDashedToCamelCase(cnnr.getAttribute(ATTR.array));
         parentObj = parentObj[key] = [];
 
     for (var i = 0; i < children.length && !stop; i++) {
@@ -289,9 +408,9 @@ FlatJS.Component = FlatJS.Widget.extend(function() {
 
   function constructJSONfromNode(child, parentObj, isModel, parentIsArray) {
     var isObj = child.hasAttribute(ATTR.object),
-        key   = convertCamelCase(child.getAttribute(isObj ? ATTR.object : ATTR.array));
+        key   = FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(isObj ? ATTR.object : ATTR.array));
     if (isModel) {
-      var modelClass = findFn(child.getAttribute(ATTR.resource)),
+      var modelClass = FlatJS.Helpers.findFunctionByString(FlatJS.Helpers.convertDashedToCamelCase(child.getAttribute(ATTR.resource))),
           obj        = modelClass ? modelClass.find(child.getAttribute(ATTR.id)) : false;
       if (obj && parentIsArray) {
         parentObj.push(obj);
