bb04f250e39d9a33459c21766908d6be2926aafc
Sankho Mallik <sankho.mallik@gmail.com>
2014-07-05 15:51:39 -0400

    refactoring FlatJS.Classy and include code comments for documentation

--- src/FlatJS.Classy.js
+++ src/FlatJS.Classy.js
@@ -1,127 +1,85 @@
+// defines FlatJS just in case, opens an initial scope.
 var FlatJS = FlatJS || {};
+(function() {
 
-FlatJS.Classy = (function() {
-
+  // Some private static variables. Two arrays are used to store all private functions + psuedo private variables used by FlatJS.Classy.prototype._ (see below). Initializing and fnTest imported from John Resig - see comments below.
   var fnStore  = [],
       varStore = [],
       initializing = false;
       fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-  function handlePrivateVariableStorage(key, val) {
-    var store = varStore[this] = varStore[this] || [];
-    store = store[key] = store[key] || [];
-    this.varcnt = this.varcnt || store.length + 1;
-
-    store[this.varcnt] = typeof val !== 'undefined' ? val : store[this.varcnt] || null;
-
-    return store[this.varcnt];
-  }
-
-  function isFunctionAnonymous(func) {
-    var fName;
-    var inFunc = func.toString();
-    var rExp   = /^function ([^\s]+) \(\)/;
-    if (fName = inFunc.match(rExp)) {
-      fName = fName[1];
-    }
-
-    return fName === null;
-  }
-
-  function Classy() {
+  /**
+   * # FlatJS.Classy
+   *
+   * Base Class definition used throughout FlatJS. Provides a OOP like class pattern based on John Resig's method.
+   * See http://ejohn.org/blog/simple-javascript-inheritance.
+   * Extended to provide ways to call private member functions to retain the value of "this" throughout
+   * your code, as well as a psuedo-private variable storage to save member variables without making them
+   * visible via a console.log or similiar method. See FlatJS.Classy.prototype._ below.
+   *
+   * @public
+   */
+  FlatJS.Classy = function () {
 
     /**
+     * ## FlatJS.Classy.prototype._
+     *
      * Privacy is keen
      *
+     * @instanceOf {FlatJS.Classy}
+     * @public
      * @param {Function|String} fn The function to be executed / variable name to be stored
      * @param {*} val Value of corresponding string in fn
      */
     this._ = function(fn, val) {
       if (typeof fn === 'function') {
-        // if (!isFunctionAnonymous(fn)) {
-          var self  = this,
-              store = fnStore[self] = fnStore[self] || [];
-          this.icnt = this.icnt || store.length + 1;
-          store     = store[this.icnt] = store[this.icnt] || [];
+        var self  = this,
+            store = fnStore[self] = fnStore[self] || [];
+        this.icnt = this.icnt || store.length + 1;
+        store     = store[this.icnt] = store[this.icnt] || [];
 
-          var finalVal = store[fn] = store[fn] || function() {
-            store[fn][self.icnt] = this;
-            return fn.apply(self, arguments);
-          }
-        //   console.log('not anonymous!!');
-        // } else {
-        //   console.log('anonymous!!');
-        //   var self = this;
-        //   return function() {
-        //     fn.apply(self, arguments);
-        //   }
-        // }
+        var finalVal = store[fn] = store[fn] || function() {
+          store[fn][self.icnt] = this;
+          return fn.apply(self, arguments);
+        }
       } else {
         var finalVal = this._(handlePrivateVariableStorage)(fn, val);
       }
 
       return finalVal;
-    },
-
-    this.later = function(when, fn) {
-      return setTimeout(this._(fn), when);
     }
 
-    this.every = function(when, fn) {
-      var rt  = setInterval(this._(fn), when),
-          obj = {};
-
-      obj.stop = function() {
-        clearInterval(rt);
-      }
-
-      obj.start = this._(function() {
-        return this.every(when, fn);
-      });
-
-      return obj;
-    }
-
-    this.rand = function(floor, ceil) {
-      if (!ceil) {
-        ceil = floor;
-        floor = 0;
-      }
-
-      return Math.floor(Math.random() * (ceil - floor + 1)) + floor;
-    }
+  // end FlatJS.Classy scope
   }
 
-  // Create a new Class that inherits from this class
-  Classy.extend = function(prop) {
+  /**
+   * ## FlatJS.Classy.extend
+   *
+   * Create a new Class that inherits from this class. Stolen from John Resig, see @link http://ejohn.org/blog/simple-javascript-inheritance/ for full comments on static Extend function.
+   *
+   * @public
+   * @static
+   * @param  {Object||Function} prop Either an object or a static function which returns an object representing the public API of the new class to be created.
+   * @return {Function} Returns the generated constructor.
+   */
+  FlatJS.Classy.extend = function(prop) {
 
     if (typeof prop === 'function') {
       prop = prop();
     }
 
     var _super = this.prototype;
-
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
     initializing = true;
     var prototype = new this();
     initializing = false;
 
-    // Copy the properties over onto the new prototype
     for (var name in prop) {
-      // Check if we're overwriting an existing function
       prototype[name] = typeof prop[name] == "function" &&
         typeof _super[name] == "function" && fnTest.test(prop[name]) ?
         (function(name, fn){
           return function() {
             var tmp = this._super;
-
-            // Add a new ._super() method that is the same method
-            // but on the super-class
             this._super = _super[name];
-
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
             var ret = fn.apply(this, arguments);
             this._super = tmp;
 
@@ -131,14 +89,13 @@ FlatJS.Classy = (function() {
         prop[name];
     }
 
-    // The dummy class constructor
+    // creates a "class" to be returned
     function Class() {
-      // All construction is actually done in the init method
       if (!initializing && this.init) {
         this.init.apply(this, arguments);
       }
 
-      // Add to objects array
+      // Add any instantiated children to static objects array
       Class.fjsObjects.push(this);
     }
 
@@ -149,21 +106,37 @@ FlatJS.Classy = (function() {
       }
     }
 
-    // Populate our constructed prototype object
     Class.prototype = prototype;
 
-    // Enforce the constructor to be what we expect
     Class.prototype.constructor = Class;
 
-    // And make this class extendable
     Class.extend = arguments.callee;
 
-    // Creates a static store for all created objects of Class
+    // Creates a static store for all created objects of Class, used whenever classes are instantiated (see two comments above)
     Class.fjsObjects = [];
 
     return Class;
   };
 
-  return Classy;
+  /**
+   * ### handlePrivateVariableStorage - private to FlatJS.Classy.prototype
+   *
+   * Private function handles the psuedo private variable storage method. Use like so: this._('privateVar', "oh, hello"), get like so this._('privateVar').replace('hello', 'goodbye') => "oh, goodbye". Stores values in static storage using "this" as reference key.
+   *
+   * @private
+   * @instanceOf {FlatJS.Classy}
+   * @param {String} key
+   * @param val
+   */
+  function handlePrivateVariableStorage(key, val) {
+    var store = varStore[this] = varStore[this] || [];
+    store = store[key] = store[key] || [];
+    this.varcnt = this.varcnt || store.length + 1;
 
+    store[this.varcnt] = typeof val !== 'undefined' ? val : store[this.varcnt] || null;
+
+    return store[this.varcnt];
+  }
+
+// end scope.
 }());
