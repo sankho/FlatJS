df35e534fadba782941039e8afeab7deaedda700
Sankho Mallik <sankho.mallik@gmail.com>
2014-07-26 15:32:16 -0400

    adds another test for nested variables

--- tests/Component/Component.tests.js
+++ tests/Component/Component.tests.js
@@ -74,6 +74,7 @@ __MVMockData.mockLoadedCallback = function() {
     QUnit.ok($mock.find('#root-class-swap').hasClass('true'), "fjs-class attributes work when set to listen to objects on the root of fjsData variable");
     
     mvMod.fjsData.set('header.arbitraryTopTitle2', 'Hey Hey');
+    QUnit.equal($mock.find('h5:eq(1)').text(), 'Hey Hey', 'Dom elements bound to nested variables off root fjsData variable');
     QUnit.ok($mock.find('#inner-item-class-swap').hasClass('true'), 'fjs-class attributes work when set to listen to objects nested within the root fjsData variable');
 
   });
